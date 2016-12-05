7ffa67271f829b1ea9744517112e7e6aef825463
William R. Fraser <wfraser@codewise.org>
2016-12-04 21:16:23 -0800

    day 5 python quick-n-dirty

---
+++ 05/prog.py
@@ -0,0 +1,33 @@
+import hashlib
+import sys
+
+def hash(key, n):
+    h = hashlib.md5()
+    h.update(key)
+    h.update("{}".format(n))
+    return h.hexdigest()
+
+prefix = sys.stdin.readline().rstrip()
+
+answer1 = ""
+answer2 = {}
+foundFive = False
+n = 0
+while True:
+    out = hash(prefix, n)
+    if out.startswith("00000"):
+        if len(answer1) < 8:
+            answer1 += out[5]
+        if out[5].isdigit():
+            pos = int(out[5])
+            if pos < 8 and not pos in answer2:
+                answer2[pos] = out[6]
+        print(out)
+        if len(answer2) == 8:
+            break
+    if n % 100000 == 0:
+        sys.stdout.write(".")
+        sys.stdout.flush()
+    n += 1
+print(answer1)
+print(answer2)
