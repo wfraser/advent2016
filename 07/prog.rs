2ae6585306c0a44653bae0b465f52fbd0049cdb2
William R. Fraser <wfraser@codewise.org>
2016-12-06 21:59:36 -0800

    day 7.2 rust

--- 07/prog.rs
+++ 07/prog.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn check(chars: &[char]) -> bool {
+fn check_abba(chars: &[char]) -> bool {
     for window in chars.windows(4) {
         if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
             return true;
@@ -9,47 +9,80 @@ fn check(chars: &[char]) -> bool {
     false
 }
 
+fn check_aba_bab(outsides: &[Vec<char>], insides: &[Vec<char>]) -> bool {
+    for chars in outsides {
+        for window in chars.windows(3) {
+            if window[0] == window[2] && window[0] != window[1] {
+                if check_bab(insides, window[0], window[1]) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+fn check_bab(insides: &[Vec<char>], a: char, b: char) -> bool {
+    for chars in insides {
+        for window in chars.windows(3) {
+            if window[0] == b && window[1] == a && window[2] == b {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
     let mut count1 = 0;
+    let mut count2 = 0;
     let mut line = String::new();
     while let Ok(n) = io::stdin().read_line(&mut line) {
         if n == 0 { break; }
 
         let mut in_brackets = false;
-        let mut inside = Vec::new();
-        let mut outside = Vec::new();
-        let mut valid = true;
-        let mut abbas_outside = false;
+        let mut insides  = Vec::<Vec<char>>::new();
+        let mut outsides = Vec::<Vec<char>>::new();
+        let mut buf = Vec::<char>::new();
         for c in line.chars() {
             if c == '[' || c == '\n' {
                 assert!(!in_brackets);
                 in_brackets = true;
-                if check(&outside) {
-                    abbas_outside = true;
-                }
-                outside.clear();
+                outsides.push(std::mem::replace(&mut buf, vec![]));
             } else if c == ']' {
                 assert!(in_brackets);
                 in_brackets = false;
-                if check(&inside) {
-                    valid = false;
-                    break;
-                }
-                inside.clear();
-            } else if in_brackets {
-                inside.push(c);
+                insides.push(std::mem::replace(&mut buf, vec![]));
             } else {
-                outside.push(c);
+                buf.push(c);
             }
         }
 
-        if valid && abbas_outside {
+        let mut valid = false;
+        for chars in &outsides {
+            if check_abba(&chars) {
+                valid = true;
+                break;
+            }
+        }
+        if valid {
+            for chars in &insides {
+                if check_abba(&chars) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if valid {
             count1 += 1;
         }
 
-        inside.clear();
-        outside.clear();
+        if check_aba_bab(&outsides, &insides) {
+            count2 += 1;
+        }
+
         line.clear();
     }
     println!("{}", count1);
+    println!("{}", count2);
 }
